2324ecf24a0e5bbd1e8c4ab08d77c2ac8b4db176
sun <lznhst@163.com>
2019-05-08 11:32:07 +0800

    增加grpc服务端的线程池大小限制

--- person_server.cpp
+++ person_server.cpp
@@ -1,10 +1,17 @@
+#include <grpcpp/resource_quota.h>
 #include "person_manage.h"
+
+// maxThreadNum 根据计算机硬件设置
+const int maxThreadNum = 20;
 
 void RunServer() {
     std::string server_address("localhost:50001");
     PersonService service;
 
     grpc::ServerBuilder builder;
+    grpc::ResourceQuota quota;
+    quota.SetMaxThreads(maxThreadNum);
+    builder.SetResourceQuota(quota);
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
